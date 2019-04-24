bcdade1bec307b66f46f9d153af067a491304b44
Sarah Nurwidhiafitri Sukamto <sarsu@Sarahs-MacBook-Pro.local>
2019-04-24 20:21:51 +1000

    created navbar,login-page,main-menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginPageComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule
