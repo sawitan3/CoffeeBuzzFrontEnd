84199d13571a4a0f5df7db86e8c1720b01f14fdd
Ignatia Stellarista <clarisarose@ymail.com>
2019-04-26 19:51:02 +1000

    set up navbar navigation, added cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { CartComponent } from './cart/cart.component';
+
+const appRoutes: Routes = [
+  {path:"login", component: LoginPageComponent},
+  {path:"cart", component: CartComponent},
+  {path:"**", component: MainMenuComponent}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LoginPageComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   isNavbarCollapsed = true;
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  linkClick(destination: string){
+  	this.router.navigateByUrl(destination);
+  	this.isNavbarCollapsed = true;
   }
 
 }
