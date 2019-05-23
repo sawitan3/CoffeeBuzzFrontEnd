3d0cfc0cba384f3ed727236ae5c2a807c894dbc1
Ignatia Stellarista <clarisarose@ymail.com>
2019-05-23 20:04:08 +1000

    form validation on create new barista page

--- src/app/admin-page/create-new-barista/create-new-barista.component.ts
+++ src/app/admin-page/create-new-barista/create-new-barista.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RegisterData, RegisterService} from '../../register.service';
 import {Router} from '@angular/router';
 import {NgbActiveModal, NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-create-new-barista',
@@ -11,6 +12,10 @@ import {NgbActiveModal, NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
 export class CreateNewBaristaComponent implements OnInit {
 
   user: RegisterData = { username: '', email: '', role: '2', password: ''};
+
+  confirm = '';
+
+  error;
 
   constructor(
       private registerService: RegisterService,
@@ -22,7 +27,20 @@ export class CreateNewBaristaComponent implements OnInit {
 
   onSubmit() {
     this.registerService.addUser(this.user).subscribe(
-        () => {this.modal.close(); window.location.reload(); }
+        () => {this.modal.close(); window.location.reload(); },
+        (err) => {this.onError(err); }
     );
+  }
+
+  onError(error: HttpErrorResponse) {
+    console.log(error);
+    this.error = {type: '', message: ''};
+    if (error.status === 422) {
+      this.error.message = 'Username is already used.';
+      this.error.type = 'info';
+    } else if (error.status === 0 || error.status === 500 || error.status === 404) {
+      this.error.message = 'Our server encountered a problem. Please try again.';
+      this.error.type = 'info';
+    }
   }
 }
